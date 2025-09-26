2c57e10bdf5e5a432592352b8678b3d3490b6401
Rob Nodz <robert.nodzewski@mediatel.co.uk>
2025-09-26 16:32:13 +0100

    replacomg deprecated lsp config usage

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -8,7 +8,7 @@ return {
     {
         "mason-org/mason-lspconfig.nvim",
         opts = {
-            ensure_installed = { "lua_ls", "ts_ls", "intelephense", "jsonls", "marksman", "html" },
+            ensure_installed = { "lua_ls", "ts_ls", "intelephense", "jsonls", "marksman", "html", "pylsp" },
         },
         dependencies = {
             { "mason-org/mason.nvim", opts = {} },
@@ -18,50 +18,45 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
-            local lspconfig = require("lspconfig")
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.ts_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.marksman.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.pylsp.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.html.setup({
-                capabilities = capabilities,
-            })
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            --                        lspconfig.intelephense.setup({
-            --                          capabilities = capabilities,
-            --                    })
-            --
-            -- let's format every php file on save
+            -- configure servers
+            local servers = { "lua_ls", "ts_ls", "marksman", "pylsp", "html" }
+
+            for _, server in ipairs(servers) do
+                vim.lsp.config(server, {
+                    capabilities = capabilities,
+                })
+            end
+
+            -- enable them
+            vim.lsp.enable(servers)
+
+            -- format on save
             vim.api.nvim_create_autocmd("BufWritePre", {
-                pattern = { "*.php", "*.html", "*.py" },
+                pattern = { "*.php", "*.html", "*.py", "*.lua" },
                 callback = function()
                     vim.lsp.buf.format({ async = false })
                 end,
             })
-            --            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})a
-            --            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            --            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-            --            vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-            local bufopts = { noremap = true, silent = true }
-            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-            vim.keymap.set('n', 'gk', vim.lsp.buf.hover, bufopts)
-            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-            vim.keymap.set('n', 'gK', vim.lsp.buf.signature_help, bufopts)
-            vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-            vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-            vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-            vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-            vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+
+            -- keymaps (applied globally via LspAttach event)
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(ev)
+                    -- local opts = { buffer = ev.buf, noremap = true, silent = true }
+                    local opts = { noremap = true, silent = true }
+                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                    vim.keymap.set('n', 'gk', vim.lsp.buf.hover, opts)
+                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                    vim.keymap.set('n', 'gK', vim.lsp.buf.signature_help, opts)
+                    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+                    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+                    -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
+                end,
+            })
         end
     }
 }
